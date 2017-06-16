5c785f2c3ee60067e8dfaf5a36e942c073c617ad
Soumyadeep Jana <sjana646@gmail.com>
2017-06-16 19:45:53 +0530

    added new message and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,46 @@
+            /*var socket = io();
+
+            socket.on("connect",function () 
+            {
+                console.log("Connected to server");
+                socket.emit("createEmail",
+                {
+                    to:"sjana646@gmail.com",
+                    text:"whats up?",
+                    timestamp:"6:20" 
+                });
+            });
+
+            socket.on("disconnect",function () 
+            {
+                console.log("Disconnected from server");
+            });
+
+            socket.on("newEmail",function (email) 
+            {
+                console.log(JSON.stringify(email,undefined,2));
+            });*/
+
+
+var socket = io();
+
+socket.on("connect",function ()
+{
+    console.log("Connected to server");
+
+    socket.on("newMessage",function (message) 
+    {
+         console.log(JSON.stringify(message,undefined,2));
+    })
+
+    socket.emit("createMessage",
+    {
+        to:"bittu",
+        text:"hey bro!"
+    });
+});
+
+socket.on("disconnect",function () 
+{
+    console.log("Disconnected from server");
+});
--- server/server.js
+++ server/server.js
@@ -13,15 +13,50 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on("connection",(socket) => 
+/*io.on("connection",(socket) => 
 {
     console.log("New user connnected");
+
+    socket.emit("newEmail",
+    {
+        from:"sumitjna23@gmail.com",
+        text:"call me ASAP",
+        timestamp:"12:50"
+    });
+
+    socket.on("createEmail",(email) => 
+    {
+        console.log(JSON.stringify(email,undefined,2));
+    });
 
     socket.on("disconnect",() => 
     {
        console.log("User disconnected");
     });
+});*/
+
+io.on("connect",(socket) => 
+{
+    console.log("New user connected");
+
+    socket.emit("newMessage",
+    {
+        from:"jana",
+        text:"hii",
+        createdAt:123
+    });
+
+    socket.on("createMessage",(message) => 
+    {
+        console.log(JSON.stringify(message,undefined,2));
+    });
+
+    socket.on("disconnect",() => 
+    {
+        console.log("User disconnected");
+    });
 });
+
 
 
 
