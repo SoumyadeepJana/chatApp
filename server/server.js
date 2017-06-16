36e155d5859588df5a004e632a0c278f09c9de4a
Soumyadeep Jana <sjana646@gmail.com>
2017-06-16 08:06:36 +0530

    package.json file configured

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const path = require("path");
 
+const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname,"../public");
 
 var app = express();
 
 app.use(express.static(publicPath));
 
-app.listen(3000,() => 
+app.listen(port,() => 
 {
-    console.log("Server up annd running on port 3000");
+    console.log(`Server is up and running on port ${port}`);
 });
