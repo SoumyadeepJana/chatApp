2bc38fa90e44da545fe19c6e4cd54dd9604b1a11
Soumyadeep Jana <sjana646@gmail.com>
2017-06-16 08:02:10 +0530

    server and index files configured

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const publicPath = path.join(__dirname,"../public");
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000,() => 
+{
+    console.log("Server up annd running on port 3000");
+});
