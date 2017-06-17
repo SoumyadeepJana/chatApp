2f580c07692e845c734a33849ea8a2ff2a07e7da
Soumyadeep Jana <sjana646@gmail.com>
2017-06-18 00:00:33 +0530

    format timestamps using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -18,9 +18,10 @@ socket.on("disconnect",function ()
 
  socket.on("newMessage",function (message) 
  {
+     var timeStamp = moment(message.createdAt).format("h:mm a");
      console.log(JSON.stringify(message,undefined,2));
      var li = jQuery("<li></li>")
-     li.text(`${message.from} : ${message.text}`);
+     li.text(`${message.from} [${timeStamp}] : ${message.text}`);
      jQuery("#messages").append(li);
  });
 
@@ -36,27 +37,28 @@ socket.on("disconnect",function ()
 jQuery("#message-form").on("submit",function (e) 
 {
     e.preventDefault();
+    var messageTextBox = jQuery("[name=message]");
     socket.emit("createMessage",
     {
         from:"USER",
-        text:jQuery("[name=message]").val()
+        text:messageTextBox.val()
     },function() 
     {
-        jQuery("[name=message]").remove();
-        jQuery("#send").remove();
+        messageTextBox.val("");
+        /*jQuery("#send").remove();
         var input = jQuery("<input type=text placeholder=Message name=message>") ;
         var button = jQuery("<button id=send>Send</button>");
         jQuery("#message-form").append(input);
-        jQuery("#message-form").append(button);
+        jQuery("#message-form").append(button);*/
     });
 });
 
 socket.on("newLocationMessage", function (locationMessage) 
 {
-    //var anchor = jQuery("<a href=locationMessage.url alt=location>User's location</a>");
+    var timeStamp = moment(locationMessage.createdAt).format("h:mm a");
     var li = jQuery("<li></li>");
     var anchor = jQuery('<a target=_blank>Location</a>');
-    li.text(`${locationMessage.from}: `);
+    li.text(`${locationMessage.from} [${timeStamp}]: `);
     anchor.attr("href",`${locationMessage.url}`);
     li.append(anchor);
     jQuery("#messages").append(li);
@@ -68,6 +70,10 @@ locationButton.on("click",function()
     if(!navigator.geolocation)
        return alert("Your browser does not support geolocation");
 
+    var locationButton = jQuery("#send-location");
+    locationButton.text("Sending...");
+    locationButton.attr("disabled");
+
     navigator.geolocation.getCurrentPosition(function (position) 
     {
         //console.log(position);
@@ -76,9 +82,12 @@ locationButton.on("click",function()
             latitude:position.coords.latitude,
             longitude:position.coords.longitude
         });
+        locationButton.text("Send Location");
+        
 
     },function () 
     {
         alert("Unable to fetch location");
+        locationButton.text("Send Location");
     });
 });
